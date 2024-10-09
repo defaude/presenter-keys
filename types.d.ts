bb3da129dff0d35e33f453d17cba78ead2399426
defaude <defaude@gmail.com>
2024-10-09 23:39:37 +0200

    (fix) fix types
    Now TS won't force you to provide all callbacks (they were optional all along, anyway)

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,4 @@
 type CallbackName = 'prev' | 'next' | 'run' | 'blank';
-type VoidFn = () => void;
 
 /**
  * Registers the given callbacks to the appropriate presenter key events.
@@ -7,4 +6,4 @@ type VoidFn = () => void;
  * @param callbacks Record of callbacks to register.
  * @param element HTML element to which the events shall be registered to. Optional, defaults to the document's body.
  */
-export declare function presenterKeys(callbacks: Record<CallbackName, VoidFn>, element?: HTMLElement): void;
+export declare function presenterKeys(callbacks: Partial<Record<CallbackName, () => void>>, element?: HTMLElement): void;
